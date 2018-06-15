f2889f0c13eeba9d9660863db34ea31fe25ef44f
Scott Fraser <scottafraser@yahoo.com>
2018-06-15 11:18:35 -0700

    bulding prototypes

--- js/scripts.js
+++ js/scripts.js
@@ -1,38 +1,46 @@
 function Pizza (size, topping){
-  this.size = size
-  this.topping = []
+  this.size = size;
+  this.topping = [];
 }
 
-// Pizza.prototype.pizzaOrder = function() {
-//   return this.size + "," this.topping [],
-// }
+Pizza.prototype.pizzaOrder = function() {
+  return this.size + "," + this.topping ;
+}
 
 
-function pizzaSize() {
+Pizza.prototype.pizzaSize = function() {
   var size = $("input[type='radio'][name='size']:checked").val()
 return size
 console.log(size);
 }
 
+Pizza.prototype.toppingChoice = function() {
+  var toppingArray = []
+  $("input[name='array']:checked").each(function ()
+  {
+      toppingArray.push($(this).val());
+  });
+  return toppingArray
+}
+
 $(document).ready(function() {
 
- var pizza = new Pizza ('size', '[toppings]')
+// var size =
+var pizza = new Pizza ('size, toppings')
 
   $(".radio").click(function(event){
-    var size = $("input[type='radio'][name='size']:checked").val()
+    var size = pizza.pizzaSize();
     console.log(size);
   });
 
   $("#toppingList").click(function(event){
-    var toppingArray = []
-    $("input[name='array']:checked").each(function ()
-    {
-        toppingArray.push($(this).val());
-    });
-    console.log(toppingArray);
+    var toppings = pizza.toppingChoice();
+    console.log(toppings);
   });
-//
-// $("#submit").click(function() {
-//   event.preventDefault();
+
+$("#submit").click(function() {
+  event.preventDefault();
+  $('#order').append(pizza.pizzaOrder())
 
   });
+});
